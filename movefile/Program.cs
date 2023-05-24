bc66564cf1d35e95db4c02eb15922c9f9340ac17
Capricorn <Capricorn@DESKTOP-DBG7PJB>
2023-05-24 11:43:34 +0530

    Add project files.

---
+++ movefile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.IO;
+
+namespace movefile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceFileName = @"D:\Move1\Test.txt.txt";
+            string destFileName = @"D:\Move2\Test.txt.txt";
+            File.Move(sourceFileName, destFileName);
+        }
+    }
+}
